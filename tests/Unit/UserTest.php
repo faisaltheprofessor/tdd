ee5ad9a7524a9b791a76c5c01b0d02f89ee2c2e4
Faisal Khan <mahboobshah.faisal@gmail.com>
2024-02-07 01:29:36 +0100

    user unit test

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic unit test example.
+     */
+    public function test_a_user_has_projects(): void
+    {
+      $user =  User::factory()->create();
+
+      $this->assertInstanceOf(HasMany::class, $user->projects());
+    }
+}
